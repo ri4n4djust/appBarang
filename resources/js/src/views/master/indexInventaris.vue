29e5efc27f6e583ac54025f468eefd1651761465
ri4n4djust <rian.adjust@gmail.com>
2023-03-30 17:02:42 +0800

    update

--- resources/js/src/views/master/indexInventaris.vue
+++ resources/js/src/views/master/indexInventaris.vue
@@ -55,14 +55,12 @@
                                         </a>
                                 </div>
                                 <div v-else>
-                                    <!-- {{ itemsjurnal.filter(n => n % props.row.kode_inventaris === 0) }} -->
-                                    <div v-for="jr in itemsjurnal" :key="jr">
-                                        
-                                        <div v-if="jr.rkode_inventaris === props.row.kode_inventaris ">
-                                            Sudah  {{ props.row.kode_inventaris }} = {{ jr.rkode_inventaris }}
-                                        </div>
-                                        <div v-else>
-                                            Belum Disusutkan 
+                                    
+                                    <div :set="data = itemsjurnal.filter(n => n.rkode_inventaris === props.row.kode_inventaris)">
+
+                                       
+                                        <p v-if="data.length > 0">sudah  </p>
+                                        <p v-else>belum
                                             <a href="javascript:void(0);" @click="jurnal_row(props.row)" >
                                                 <svg
                                                     xmlns="http://www.w3.org/2000/svg"
@@ -82,29 +80,12 @@
                                                     ></path>
                                                 </svg>
                                             </a>
-                                        </div>
+                                        </p>
+                                            
+
 
                                     </div>
-                                    <!-- Belum Disusutkan 
-                                        <a href="javascript:void(0);" @click="jurnal_row(props.row)" >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="24"
-                                                height="24"
-                                                viewBox="0 0 24 24"
-                                                fill="none"
-                                                stroke="currentColor"
-                                                stroke-width="2"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                class="feather feather-settings text-primary"
-                                            >
-                                                <circle cx="12" cy="12" r="3"></circle>
-                                                <path
-                                                    d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"
-                                                ></path>
-                                            </svg>
-                                        </a> -->
+                                    
                                 </div>
                                 
                                 
